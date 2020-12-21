fc702335af955b11bc485052be654e408730e783
Khiki <hikmat0495@gmail.com>
2020-12-21 22:09:52 +0500

    fixed

--- pkg/managers/service.go
+++ pkg/managers/service.go
@@ -96,7 +96,7 @@ func (s *Service) GetToken(ctx context.Context, phone string, password string) (
 	}
 
 	buffer := make([]byte, 256)
-	n, err := rand.Read(buffer)
+	n, err := rand.Read(buffer)у 
 	if n != len(buffer) || err != nil {
 		return "", ErrInternal
 	}
