53f9706d9f3fc17d79a0a2adee87079fda3f88aa
Khiki <hikmat0495@gmail.com>
2020-12-21 21:01:48 +0500

    fixed

--- docker-entrypoint-initdb.d/data.sql
+++ docker-entrypoint-initdb.d/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO managers (name, phone, password, roles)
-values ('vasya', '+992000000001', '$2a$10$oc/QUw9dRpQAtWeqrs/ma.w7gH23qJHAWDrrLI6GkYTg/b9J.YMo.', '{"MANAGER","ADMIN"}' );
+values ('vasya', '+992000000001', '$2a$10$OaUtjCNv2DT5x/dXcV.P3eYkIPIRtBr/v8Nluwifz6brSkfyXOh6m', '{"MANAGER","ADMIN"}' );
